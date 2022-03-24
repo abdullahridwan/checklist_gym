84976b7cb7fab3a4ecd519882b1a4034889641ee
Abdullah Ridwan <abdullahridwan73@icloud.com>
2022-03-24 00:14:13 -0400

    successfully created editable list in form in file TestListForm

---
+++ TheGymRatsChecklist/Main Views/TestListInForm.swift
@@ -0,0 +1,45 @@
+//
+//  TestListInForm.swift
+//  TheGymRatsChecklist
+//
+//  Created by Abdullah Ridwan on 3/24/22.
+//
+
+import SwiftUI
+
+struct TestListInForm: View {
+    @State var selectedDate: Date = Date()
+    @State var someList: [String] = ["Creatine", "Water"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                    DatePicker("Choose a Date", selection: $selectedDate, displayedComponents: .date)
+                    Section("List", content: {
+                        ForEach($someList, id:\.self){$someItem in
+                            Text(someItem)
+                        }
+                        .onMove { indexSet, offset in
+                            someList.move(fromOffsets: indexSet, toOffset: offset)
+                        }
+                        .onDelete { indexSet in
+                            someList.remove(atOffsets: indexSet)
+                        }
+                        
+                    })
+                }
+            .navigationTitle("Form test")
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing, content: {
+                    EditButton()
+                })
+            })
+        }
+    }
+}
+
+struct TestListInForm_Previews: PreviewProvider {
+    static var previews: some View {
+        TestListInForm()
+    }
+}
